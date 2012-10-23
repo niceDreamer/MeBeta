99d0abcb8fadc6348937334e63379c343bf88c7d
master <nicedreamer@qq.com>
2012-10-23 18:20:29 +0800

    [Modify Feature]
    bug fixs.

--- config/config.php
+++ config/config.php
@@ -4,7 +4,7 @@
 
 define ('DEVELOPMENT_ENVIRONMENT',true);
 
-define('DB_NAME', 'yourdatabasename');
-define('DB_USER', 'yourusername');
-define('DB_PASSWORD', 'yourpassword');
+define('DB_NAME', 'test');
+define('DB_USER', 'admin');
+define('DB_PASSWORD', '');
 define('DB_HOST', 'localhost');
